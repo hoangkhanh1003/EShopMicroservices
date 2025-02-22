3368233414b75bba7105d43c8ce5d54f3b659522
khanhnh2k <khanh200017@gmail.com>
2025-02-22 21:21:05 +0700

    commit crud Catalog

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("CatalogConnection")!);
+}).UseLightweightSessions();
+
+// Add services to the container.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
